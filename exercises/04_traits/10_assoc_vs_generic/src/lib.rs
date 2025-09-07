c458e6ba56dce0233b253acf1a5d9914dd56cf0c
lovegupta112 <loveguptaofficials@gmail.com>
2025-09-07 20:17:47 +0530

    completed tasks related to traits like copy,clone,drop,From

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -12,7 +12,25 @@
 // interested in learning more about it.
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
+pub trait Power<T,U>{
+    fn power(self,pow:U)->T;
+}
 
+impl Power<u32,u32> for u32{
+     fn power(self,pow:u32)->u32{
+         self.pow(pow)
+     }
+}
+impl Power<u32,u16> for u32{
+    fn power(self,pow:u16)->u32{
+          self.pow(pow.into())
+    }
+}
+impl Power<u32,&u32>for u32{
+     fn power(self,pow:&u32)->u32{
+       self.power(*pow)
+     }
+}      
 #[cfg(test)]
 mod tests {
     use super::Power;
--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,58 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+use std::ops::Add;
+
+#[derive(Debug,Clone,Copy)]
+pub struct SaturatingU16{
+     value:u16,
+}
+
+impl From <u8> for SaturatingU16{
+  fn from(val: u8) -> Self {
+       SaturatingU16 { value:val.into() }
+  }
+}
+impl From <u16> for SaturatingU16{
+  fn from(val: u16) -> Self {
+       SaturatingU16 { value:val }
+  }
+}
+impl From <&u8> for SaturatingU16{
+  fn from(val: &u8) -> Self {
+       SaturatingU16 { value:(*val).into() }
+  }
+}
+impl From <&u16> for SaturatingU16{
+  fn from(val: &u16) -> Self {
+       SaturatingU16 { value:(*val).into() }
+  }
+}
+impl Add<Self> for SaturatingU16{
+    type Output=Self;
+  fn add(self,other:Self)->Self::Output{
+     SaturatingU16{value:self.value.saturating_add(other.value)}
+  }
+}
+impl Add<&Self> for SaturatingU16{
+    type Output=Self;
+  fn add(self,other:&Self)->Self::Output{
+     SaturatingU16{value:self.value.saturating_add((*other).value)}
+  }
+}
+impl Add<u16> for SaturatingU16{
+    type Output=u16;
+  fn add(self,other:u16)->u16{
+    self.value.saturating_add(other)
+  }
+}
+impl PartialEq<Self> for SaturatingU16{
+ fn eq(&self, other: &Self) -> bool {
+     self.value==other.value
+ }
+}
+impl PartialEq<u16> for SaturatingU16{
+ fn eq(&self, other: &u16) -> bool {
+     self.value==*other
+ }
+}
